81e2b56e7a42c9aecfe3c798df251ebfa25d932f
RaDin <143905822+RaDinds@users.noreply.github.com>
2024-03-31 07:02:18 +0500

    class_3

---
+++ 03 Day_3 Creating Our First PHP Website/index.php
@@ -0,0 +1,12 @@
+<!-- The easiest way to serve php file on localhost is to add file in path(C:\xampp\htdocs\RaDins) folder. -->
+
+<!-- Then run Apache and MySQL Services from XAMPP Control Panel. -->
+
+<!-- In php default name of serve file on loacalhost is index.php file. -->
+
+<!-- Basic structure of php program: -->
+<?php
+// Echo is used to display something.
+echo "Hello World! ";
+echo 5 + 32;
+?>
