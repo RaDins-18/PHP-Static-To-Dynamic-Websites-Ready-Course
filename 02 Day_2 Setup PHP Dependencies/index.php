9d893202193f6e9c3b1f408d353dccce582e81f0
RaDin <143905822+RaDinds@users.noreply.github.com>
2024-03-31 07:02:11 +0500

    class_2

---
+++ 02 Day_2 Setup PHP Dependencies/index.php
@@ -0,0 +1,6 @@
+The easiest way to setup php dependencies into pc is to download and install XAMPP.
+
+
+What is XAMPP:
+
+• XAMPP is a free and open-source cross-platform web server solution stack package developed by Apache Friends.
