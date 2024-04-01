ed2e1042190a7f253940fc07d5c21f9a2dd6285b
RaDin <143905822+RaDins-18@users.noreply.github.com>
2024-04-02 02:11:46 +0500

    class_5

---
+++ 05 Day_5 Php Variables/index.php
@@ -0,0 +1,17 @@
+<?php
+// In PHP key words are not case sensitive.
+EchO "This is class_5. <br>";
+
+// Variables are containers for storing information, that starts with $ symbol.
+$name = "RaDin";
+// Variables can be number, string, float, bool, etc...
+$income = 50000.50;
+
+echo "This guy is $name and his income is $income Rupees. <br>";
+
+echo "$name is a good boy. <br>";
+echo "$name is by default programmer.";
+
+// Variables are case sensitive, that why the below line shows an error.
+echo "$Name is by default programmer.";
+?>
