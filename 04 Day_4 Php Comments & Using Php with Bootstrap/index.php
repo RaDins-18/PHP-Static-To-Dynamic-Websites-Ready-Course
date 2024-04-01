b58803d4b88129748395f7e52c04ccc40106537c
RaDin <143905822+RaDins-18@users.noreply.github.com>
2024-04-02 02:11:33 +0500

    class_4

---
+++ 04 Day_4 Php Comments & Using Php with Bootstrap/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "This is "; echo "Title"; ?></title>
+</head>
+<body>
+    <!-- We can use php directly in html -->
+    <?php
+    echo "Hello World! ";
+    ?>
+
+    <br>
+
+    <?php
+    echo 5 + 32;
+    ?>
+
+    <br>
+
+    <?php
+    echo "Single Line Comment";
+    // I am RaDin
+    ?>
+
+    <br>
+
+    <?php
+    echo "Multiline Line Comment";
+    /*
+        I
+        am
+        RaDin
+    */
+    ?>
+</body>
+</html>
